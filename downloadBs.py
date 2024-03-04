28182a15de4230d611673eaf9e43d4f5dfe8dab3
JONATHAN CASSARA <37684306+Jonathancassara@users.noreply.github.com>
2024-03-04 12:27:20 +0100

    Update downloadBs.py

--- downloadBs.py
+++ downloadBs.py
@@ -2,12 +2,17 @@ import os
 import requests
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 def download_files(url, local_path):
     if not os.path.exists(local_path):
         os.makedirs(local_path)
     
     response = requests.get(url)
+    if response.status_code != 200:
+        print(f"Erreur lors de l'accès à {url}: Statut {response.status_code}")
+        return
+
     soup = BeautifulSoup(response.text, 'html.parser')
     
     for link in soup.select("a[href]"):
@@ -21,11 +26,18 @@ def download_files(url, local_path):
             file_path = os.path.join(local_path, href)
             print(f"Téléchargement de {file_url} vers {file_path}")
             with requests.get(file_url, stream=True) as r:
-                with open(file_path, 'wb') as f:
-                    for chunk in r.iter_content(chunk_size=8192):
-                        f.write(chunk)
+                if r.status_code == 200:
+                    with open(file_path, 'wb') as f:
+                        for chunk in r.iter_content(chunk_size=8192):
+                            f.write(chunk)
+                else:
+                    print(f"Erreur lors du téléchargement de {file_url}: Statut {r.status_code}")
 
 if __name__ == "__main__":
     url = input("Entrez l'URL du répertoire à télécharger: ")
-    local_path = input("Entrez le chemin du dossier local pour enregistrer les fichiers: ")
+    
+    # Création du chemin basé sur la date du jour
+    date_str = datetime.now().strftime("%Y-%m-%d")
+    local_path = os.path.join("C:/Temp", date_str)
+    
     download_files(url, local_path)
